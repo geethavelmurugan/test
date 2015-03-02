819bbc192a0f4e76b5bab21142206ab6bf481a12
geethavelmurugan <geethamca.gs@gmail.com>
2015-03-02 18:13:02 +0530

    'commit for sample

---
+++ advertisement/admin.py
@@ -0,0 +1,37 @@
+from advertisement.models import *
+from django.contrib import admin
+
+
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(SubCategory)
+admin.site.register(Mobiles)
+admin.site.register(Mobilebrand)
+admin.site.register(Mobilemodel)
+admin.site.register(Os)
+admin.site.register(Sim)
+admin.site.register(Mobileinclude)
+admin.site.register(Tablets)
+admin.site.register(Tabletbrand)
+admin.site.register(Access)
+admin.site.register(Accesstype)
+admin.site.register(Accessbrand)
+
+admin.site.register(Cars)
+admin.site.register(Year)
+admin.site.register(Color)
+admin.site.register(Fueltype) 
+admin.site.register(Carbrand)
+admin.site.register(Carmodel)
+admin.site.register(Motorbrand)
+admin.site.register(Motormodel)
+admin.site.register(Scooterbrand)
+admin.site.register(Scootermodel)
+admin.site.register(Spare_Producttype)
+admin.site.register(Buses_Vehicletype)
+admin.site.register(Construction_Vehicletype)
+
+admin.site.register(City)
+admin.site.register(Locality)
+# admin.site.register(Note)
